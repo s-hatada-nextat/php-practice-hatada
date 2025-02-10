d2556bff89609053335d59ed3d36b66bb53e1caa
nextat_staff <nextat_staff@adminnoMacBook-Pro.local>
2025-02-10 14:07:46 +0900

    PHP入門課題

--- public/index.php
+++ public/index.php
@@ -1,12 +1,26 @@
 <?php
+$j = 1;
+while ($j <= 100) {
+    if ($j % 15 === 0){
+        echo('FizzBuzz,<pre>');
+    }elseif ($j % 5 === 0){
+        echo('Buzz,<pre>');
+    }elseif ($j % 3 === 0){
+        echo('Fizz,<pre>');
+    }else {
+        echo("$j,<pre>");
+    }
+    $j++;
+}
+
 for ($i = 1; $i <= 100; $i++){
     if ($i % 15 === 0){
-        echo("FizzBuzz,");
+        echo('FizzBuzz,<pre>');
     }elseif ($i % 5 === 0){
-        echo("Buzz,");
+        echo('Buzz,<pre>');
     }elseif ($i % 3 === 0){
-        echo("Fizz,");
+        echo('Fizz,<pre>');
     }else {
-        echo("$i,");
+        echo("$i,<pre>");
     }
 }
